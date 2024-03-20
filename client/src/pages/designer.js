740f0419033ae443c0a2c71a8bcc9b270ca807f8
Josie <jqualls@mail.bradley.edu>
2024-03-20 15:51:52 -0500

    Random link added

--- client/src/pages/designer.js
+++ client/src/pages/designer.js
@@ -4,6 +4,11 @@ const DesignerLinks = () => {
     return (
         <div>
             <h1>Designer Links</h1>
+            <body>
+                This is where the links to the designers will go until we have products to put them under!<br></br>
+                <br></br>
+                <a href="https://www.hacksmith.com/">HackSmith Industries Page</a>
+            </body>
         </div>
     );
 };
