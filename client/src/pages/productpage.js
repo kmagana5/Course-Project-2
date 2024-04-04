d4810a06f57e597ad7a6fc3d8ed006e34c5b83d6
“sperez777” <sperez@mail.bradley.edu>
2024-04-03 20:33:46 -0500

    Fixed issues from Class

--- client/src/pages/productpage.js
+++ client/src/pages/productpage.js
@@ -23,24 +23,14 @@ const ProductPage = () => {
     <h1>ProductPage</h1>
     <div style={{ display: "flex", flexDirection: "row" }}>
         <div>
-<<<<<<< HEAD
-            <img src={product.product_image} alt="Product Image"/>
+        <img src="/house.jpg" alt="Product Image" style={{padding:"50px"}}/>
             <h2>From the Designer</h2>
         </div>
         <div style={{ marginLeft: "10px" }}>
             <h2>3D House Product Sample</h2>
-            <p>{product.description}</p>
             <p> By: <a href="https://www.hacksmith.com/" target="_blank">HackSmith Industries Page</a> </p>
             <br></br>
-            <button style={{ marginTop: "10px" }}>Add to Cart</button>
-=======
-            <img src="/house.jpg" alt="Product Image"/>
-        </div>
-        <div style={{ marginLeft: "10px" }}>
-            <h2>3D House Product Sample</h2>
-            <p>This is a 3D house</p>
-            <button style={{ marginTop: "10px" }} onClick={onSubmit}>Add to Cart</button>
->>>>>>> Seb_insert
+            <button style={{ marginTop: "10px" }} onClick={onSubmit} >Add to Cart</button>
         </div>
     </div>
     <div style={{width: "50%"}}>
@@ -57,7 +47,7 @@ const ProductPage = () => {
             <h2>Virtual Walkthrough</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo eu diam sit amet semper. In aliquam faucibus sem, sed vestibulum nunc rhoncus a. In fringilla magna sit amet consequat congue. Mauris quis maximus eros. Aenean ultricies enim eu quam consequat tincidunt. Praesent luctus blandit quam nec condimentum. Suspendisse fermentum libero nulla, id mollis urna commodo quis. Donec velit nunc, semper et porttitor nec, feugiat in felis.</p>
         </div>
-        <img src={product.product_image} alt="Product Image" style={{padding:"50px"}}/>
+        
 
     </div>
     </div>
