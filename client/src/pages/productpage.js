28e6fea7cb06e182c3913d8cb5dddb35e993e2ff
tringuyen0208 <h004.thanhhd1@gmail.com>
2024-05-07 21:24:42 -0500

    add show more show less button to product page

--- client/src/pages/productpage.js
+++ client/src/pages/productpage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Rating from "../components/Rating";
 import "../styles/Rating.css";
 import ScrollMenu from "../components/ScrollMenu";
@@ -8,6 +8,9 @@ import axios from 'axios';
 
 const ProductPage = () => {
     const [showNotification, setShowNotification] = useState(false);
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen1, setIsOpen1] = useState(false);
 
     const [quantity, setQuantity] = useState(1);
    
@@ -120,9 +123,17 @@ const ProductPage = () => {
                 </div>
             </div>
             <div style={{ width: "50%" }}>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo eu diam sit amet semper. In aliquam faucibus sem, sed vestibulum nunc rhoncus a. In fringilla magna sit amet consequat congue. Mauris quis maximus eros.
+                <p 
+                  style={isOpen ? null : {
+                    WebkitLineClamp: 2,
+                    WebkitBoxOrient: 'vertical',
+                    overflow: 'hidden',
+                    display: '-webkit-box',
+                  }}
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo eu diam sit amet semper. In aliquam faucibus sem, sed vestibulum nunc rhoncus a. In fringilla magna sit amet consequat congue. Mauris quis maximus eros.
                      Aenean ultricies enim eu quam consequat tincidunt. Praesent luctus blandit quam nec condimentum.
                      Suspendisse fermentum libero nulla, id mollis urna commodo quis. Donec velit nunc, semper et porttitor nec, feugiat in felis.</p>
+                <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Show less...' : 'Show more...' }</button>
                 <h2>Home Features</h2>
                 <h2>Plan Details</h2>
                 <h2>Contractors in Your Area for this Design</h2>
@@ -132,7 +143,15 @@ const ProductPage = () => {
             <div style={{ display: "flex" }}>
                 <div style={{ width: "50%" }}>
                     <h2>Virtual Walkthrough</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo eu diam sit amet semper. In aliquam faucibus sem, sed vestibulum nunc rhoncus a. In fringilla magna sit amet consequat congue. Mauris quis maximus eros. Aenean ultricies enim eu quam consequat tincidunt. Praesent luctus blandit quam nec condimentum. Suspendisse fermentum libero nulla, id mollis urna commodo quis. Donec velit nunc, semper et porttitor nec, feugiat in felis.</p>
+                    <p 
+                      style={isOpen1 ? null : {
+                        WebkitLineClamp: 2,
+                        WebkitBoxOrient: 'vertical',
+                        overflow: 'hidden',
+                        display: '-webkit-box',
+                      }}
+                    >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo eu diam sit amet semper. In aliquam faucibus sem, sed vestibulum nunc rhoncus a. In fringilla magna sit amet consequat congue. Mauris quis maximus eros. Aenean ultricies enim eu quam consequat tincidunt. Praesent luctus blandit quam nec condimentum. Suspendisse fermentum libero nulla, id mollis urna commodo quis. Donec velit nunc, semper et porttitor nec, feugiat in felis.</p>
+                    <button onClick={() => setIsOpen1(!isOpen1)}>{isOpen1 ? 'Show less...' : 'Show more...' }</button>
                 </div>
             </div>
 
